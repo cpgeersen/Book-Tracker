ab286838ff2ec32049a8381257f788ed4cf009ea
Collin Geersen <cpgeersen@my.waketech.edu>
2026-02-05 22:44:30 -0500

    add: test for pytest environment

---
+++ tests/test_pytest_environment.py
@@ -0,0 +1,8 @@
+def test_basic_multiplication():
+    """
+        Makes sure that the current pytest environment is currently working
+        with a basic math assertion.
+
+        :return: boolean
+    """
+    assert 2 * 2 == 4
