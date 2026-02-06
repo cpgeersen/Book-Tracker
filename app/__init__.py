9879c47e1cd90410de9e96d476d2184ce0773d1d
Collin Geersen <cpgeersen@my.waketech.edu>
2026-02-05 22:58:57 -0500

    add: test flask instance creation

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+def create_app():
+    # Creates the most basic Flask instance to test
+    return app
