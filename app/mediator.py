3f4f9415ba2717790833cd7ba457b182570b4971
Collin Geersen <cpgeersen@my.waketech.edu>
2026-02-12 14:07:57 -0500

    add: basic json support for mediator

--- app/mediator.py
+++ app/mediator.py
@@ -1,10 +1,15 @@
-from flask import jsonify
 # Module to mediate calls from the frontend and backend
 # Will take JSON from frontend and call relevant query functions and return the result as JSON to frontend
 
 # POST - Takes JSON as input
 def create(json):
-    return json
+    # Can directly get values from keys
+    number_of_chapters = json['chapters']
+    # So just focus on parsing all json values from the book JSON and validating them
+    # i.e. ISBN should be 13 numbers, etc.
+
+    # Just demonstrating, can remove this
+    return number_of_chapters
 
 
 # GET - Takes JSON as input
