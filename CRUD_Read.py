651b031edd01d1f6ea96cda50aee5293d3478d04
Collin Geersen <cpgeersen@my.waketech.edu>
2026-02-16 10:59:20 -0500

    update: CRUD read to return value rather than print

--- CRUD_Read.py
+++ CRUD_Read.py
@@ -1,15 +1,17 @@
 import sqlite3
-# This file contains Read functionality for book, author, publisher, and genre records. 
+
+
+# This file contains Read functionality for book, author, publisher, and genre records.
 # Uses ISBN as parameter. 
 
-# Read functionaligy for book records.  
+# Read functionality for book records.
 def read_book(x):
     try:
-        # Connect to SQLite database. 
+        # Connect to SQLite database.
         conn = sqlite3.connect("bt.db")
         cursor = conn.cursor()
 
-        # Query database for ISBN provided. 
+        # Query database for ISBN provided.
         read_query = "SELECT * FROM Books WHERE ISBN = ?"
         criteria = (x,)
         cursor.execute(read_query, criteria)
@@ -18,23 +20,23 @@ def read_book(x):
 
         # Check to confirm value found in table
         if result:
-            return print(result)
+            return result
         else:
-            return print("ISBN not found")
-        
+            return "ISBN not found"
+
     except sqlite3.Error as error:
         print(f"Database error: {error}")
         conn.close()
 
 
-# Read functionaligy for author records (returns author's first and last name).  
+# Read functionality for author records (returns author's first and last name).
 def read_author(x):
     try:
-        # Connect to SQLite database. 
+        # Connect to SQLite database.
         conn = sqlite3.connect("bt.db")
         cursor = conn.cursor()
 
-        # Query database for ISBN provided. 
+        # Query database for ISBN provided.
         read_query = "SELECT Author_First_Name, Author_Last_Name FROM Authors as A JOIN BookAuthor as B ON A.AuthorID = B.AuthorID WHERE ISBN = ?"
         criteria = (x,)
         cursor.execute(read_query, criteria)
@@ -43,23 +45,23 @@ def read_author(x):
 
         # Check to confirm value found in table
         if result:
-            return print(result)
+            return result
         else:
-            return print("ISBN not found")
-        
+            return "ISBN not found"
+
     except sqlite3.Error as error:
         print(f"Database error: {error}")
         conn.close()
 
 
-# Read functionaligy for publisher records (returns publisher's name).
+# Read functionality for publisher records (returns publisher's name).
 def read_publisher(x):
     try:
-        # Connect to SQLite database. 
+        # Connect to SQLite database.
         conn = sqlite3.connect("bt.db")
         cursor = conn.cursor()
 
-        # Query database for ISBN provided. 
+        # Query database for ISBN provided.
         read_query = "SELECT P.Publisher_Name FROM Publishers as P JOIN Books as B ON P.PublisherID = B.PublisherID WHERE B.ISBN = ?"
         criteria = (x,)
         cursor.execute(read_query, criteria)
@@ -68,23 +70,23 @@ def read_publisher(x):
 
         # Check to confirm value found in table
         if result:
-            return print(result)
+            return result
         else:
-            return print("ISBN not found")
-        
+            return "ISBN not found"
+
     except sqlite3.Error as error:
         print(f"Database error: {error}")
         conn.close()
 
 
-# Read functionaligy for genre records (returns genre).
+# Read functionality for genre records (returns genre).
 def read_genre(x):
     try:
-        # Connect to SQLite database. 
+        # Connect to SQLite database.
         conn = sqlite3.connect("bt.db")
         cursor = conn.cursor()
 
-        # Query database for ISBN provided. 
+        # Query database for ISBN provided.
         read_query = "SELECT Genre FROM Genre as G JOIN BookGenre as B on G.GenreID = B.GenreID WHERE B.ISBN = ?"
         criteria = (x,)
         cursor.execute(read_query, criteria)
@@ -93,10 +95,10 @@ def read_genre(x):
 
         # Check to confirm value found in table
         if result:
-            return print(result)
+            return result
         else:
-            return print("ISBN not found")
-        
+            return "ISBN not found"
+
     except sqlite3.Error as error:
         print(f"Database error: {error}")
         conn.close()
