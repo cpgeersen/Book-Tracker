5e74dc9f709a99553f063e526b53e8e41962659b
Collin Geersen <cpgeersen@my.waketech.edu>
2026-02-16 19:03:05 -0500

    update: CRUD read to use case statements and a wrapper function

--- cli_app/CRUD_Read.py
+++ cli_app/CRUD_Read.py
@@ -2,6 +2,22 @@ import sqlite3
 # This file contains Read functionality for book, author, publisher, and genre records. 
 # Uses ISBN as parameter. 
 
+
+# Wrapper function to interface with the other functions
+def read_from_database(user_search, read_type):
+    match read_type:
+        case 'read_book':
+            return read_book(user_search)
+        case 'read_author':
+            return read_author(user_search)
+        case 'read_publisher':
+            return read_publisher(user_search)
+        case 'read_genre':
+            return  read_genre(user_search)
+        case _:
+            raise ValueError('Not a function')
+
+
 # Read functionality for book records.
 def read_book(x):
     try:
