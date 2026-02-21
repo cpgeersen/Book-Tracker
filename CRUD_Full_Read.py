d7e81d8af3fbd483948c802237d5063de3bb3d85
Holly Green <hmgreen@my.waketech.edu>
2026-02-21 18:56:48 -0500

    Another update for CRUD_FULL_Read
    Added functions for full book record

--- CRUD_Full_Read.py
+++ CRUD_Full_Read.py
@@ -24,7 +24,7 @@ def read_book_table(ISBN_value):
                 "ISBN": result[0][0],
                 "title": result[0][1],
                 "publish_date": result[0][2],
-                "publsiher_id": result[0][3],
+                "publisher_id": result[0][3],
                 "summary": result[0][4],
                 "tag_id": result[0][5],
                 "chapters": result[0][6],
@@ -348,3 +348,162 @@ def read_genres (genre_IDs):
     except sqlite3.Error as error:
         print(f"Database error: {error}")
         conn.close()
+
+
+# Read functions to assist with creating full book record. 
+
+# Revised function for publisher name in order to have output match what is needed for reading full book record. 
+def read_publisher_name_for_full_book_record (publisher_ID):
+    try:
+        # Connect to SQLite database. 
+        conn = sqlite3.connect("bt.db")
+        cursor = conn.cursor()
+
+        # Query Publishers Table for PublisherID provided. 
+        read_query = "SELECT Publisher_Name FROM Publishers WHERE PublisherID = ?"
+        criteria = (publisher_ID,)
+        cursor.execute(read_query, criteria)
+        result = cursor.fetchall()
+        conn.close()
+
+        # Check to confirm value found in table.
+        if result:
+            convert_to_dict = {
+                "Publisher_Name": result[0][0]
+            }
+
+            json_format = json.dumps(convert_to_dict)
+            return json_format
+        else:
+            return "PublisherID not found"
+        
+    except sqlite3.Error as error:
+        print(f"Database error: {error}")
+        conn.close()
+
+
+# Revised function for author name in order to have output match what is needed for reading full book record. 
+def read_author_name_by_ISBN_full_record (ISBN_value):
+    try:
+        # Connect to SQLite database. 
+        conn = sqlite3.connect("bt.db")
+        cursor = conn.cursor()
+
+        # Query BookAuthor Table for ISBN provided. Joined with Author table to return AuthorID and author's first and last name. 
+        read_query = """
+        SELECT A.AuthorID, A.Author_First_Name, A.Author_Last_Name
+        FROM Author AS A
+        JOIN BookAuthor AS B ON A.AuthorID = B.AuthorID
+        WHERE B.ISBN = ?
+        """
+        criteria = (ISBN_value,)
+        cursor.execute(read_query, criteria)
+        result = cursor.fetchall()
+        conn.close()
+
+        # Check to confirm value found in table.
+        if result:
+            # Create dictionary for JSON formatting.
+            convert_to_dict = {}
+
+            count = 1
+
+            for index, tup in enumerate(result):
+                convert_to_dict[f"AuthorID_{index+1}"] = tup[0]
+                convert_to_dict[f"Author_First_Name_{index+1}"] = tup[1]
+                convert_to_dict[f"Author_Last_Name{index+1}"] = tup[2]
+                count += 1
+                if count > 2:
+                    break
+            
+            json_format = json.dumps(convert_to_dict)
+            return json_format
+            
+        else:
+            return "ISBN not found"
+        
+    except sqlite3.Error as error:
+        print(f"Database error: {error}")
+        conn.close()
+
+
+# Revised function for genre name in order to have output match what is needed for reading full book record. 
+def read_genres_for_full_book_record (genre_IDs):
+    try:
+        # Connect to SQLite database. 
+        conn = sqlite3.connect("bt.db")
+        cursor = conn.cursor()
+
+        # Generate the correct amount of search variables for query.
+        convert_input_from_json = json.loads(genre_IDs)
+        criteria = convert_input_from_json["GenreIDs"]
+    
+        search_values =','.join(['?'] * len(criteria))
+    
+        read_query = f"SELECT * FROM Genre WHERE GenreID IN ({search_values})"
+        cursor.execute(read_query, criteria)
+        result = cursor.fetchall()
+        conn.close()
+
+        # Check to confirm value found in table.
+        if result:
+           # Convert results into dictionary.
+            convert_to_dict = {}
+            
+            count = 1
+
+            for index, tup in enumerate(result):
+                convert_to_dict[f"GenreID_{index+1}"] = tup[0]
+                convert_to_dict[f"Genre_{index+1}"] = tup[1]
+           
+                count += 1
+                if count > 4:
+                    break
+            
+            json_format = json.dumps(convert_to_dict)
+            return json_format
+   
+        else:
+            return "GenreID not found"
+        
+    except sqlite3.Error as error:
+        print(f"Database error: {error}")
+        conn.close()
+
+
+
+# Returns full book record based on ISBN. 
+def read_full_book_record (ISBN_value): 
+
+    #Return Book table records for ISBN. 
+    read_book_table_record = read_book_table(ISBN_value)
+    converted_books = json.loads(read_book_table_record)
+
+    # Return Author Names associated with ISBN. 
+    author_names = read_author_name_by_ISBN_full_record(ISBN_value)
+    converted_auth_names = json.loads(author_names)
+
+    # Return Publisher name from an associated publisher ID in the Books record. 
+    pub_ID = converted_books["publisher_id"]
+    book_publisher = read_publisher_name_for_full_book_record(pub_ID)
+    converted_publisher = json.loads(book_publisher)
+
+    # Return all tags associated with a book record.
+    tag = converted_books["tag_id"]
+    book_tags = read_tag_table(tag)
+    converted_tags = json.loads(book_tags)
+
+    # Return Genre IDs by ISBN.  Return values passed to Read_Genre_Names.
+    read_genIDs = read_genres_IDs(ISBN_value)
+
+    # Return Genre Names by Genre IDs obtained through read_genre_IDs function for an ISBN. 
+    genre_names = read_genres_for_full_book_record(read_genIDs)
+    converted_genres = json.loads(genre_names)
+
+   # Cobmine results for all functions to create full book record. 
+    read_full_book = converted_books | converted_auth_names | converted_publisher | converted_tags | converted_genres
+
+   # Convert to JSON, printing the result "pretty".
+    read_full_book_pretty = json.dumps(read_full_book, indent=4)
+
+    return read_full_book_pretty
