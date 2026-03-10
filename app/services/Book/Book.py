072a6178960a1be3e2a75826cc962cccf54c7aea
Collin Geersen <cpgeersen@my.waketech.edu>
2026-03-10 03:42:26 -0400

    add: refactor to validate json

--- app/services/Book/Book.py
+++ app/services/Book/Book.py
@@ -2,13 +2,22 @@ from app.services.Book.BookCreate import create_book_record
 from app.services.Book.BookRead import read_full_book_record
 from app.services.Book.BookDelete import delete_book_record
 from app.services.Book.BookPredicate import is_isbn_in_book_table
+import json
 
 
 def create_book(json):
+    print('create')
     return create_book_record(json)
 
 def read_book(isbn):
-    return read_full_book_record(isbn)
+    try:
+        result = read_full_book_record(isbn)
+        print('read')
+        return result
+    except json.decoder.JSONDecodeError:
+        return f'Error: Book with ISBN {isbn} not in database.'
+
+
 
 def is_isbn_present(isbn):
     return is_isbn_in_book_table(isbn)
@@ -16,5 +25,3 @@ def is_isbn_present(isbn):
 def delete_book(isbn):
     return delete_book_record(isbn)
 
-if __name__ == '__main__':
-    pass
--- app/services/validate_book_json.py
+++ app/services/validate_book_json.py
@@ -1,5 +1,4 @@
-import json
-
+from app.services.Book.Book import create_book, read_book
 
 normal_data = {"ISBN": "0061091464",
                "Title": "The Thief of Always",
@@ -138,11 +137,175 @@ def validate_book(json, create_type):
 
 
 
-result = validate_book(normal_data, 'Book')
+#result = validate_book(normal_data, 'Book')
+#print(result)
+def add_missing_keys(json_input):
+    # Adds potentially missing keys for scenarios:
+    #   o Optional frontend fields
+    #   o Data not pulled from OL
+    #   o Unimportant data is missing from OL
+    # This function is useful since it makes some error checking easy later
+    json_input.setdefault('Chapters', '')
+    json_input.setdefault('Chapters_Completed', '')
+    json_input.setdefault('Summary', '')
+    #json_input.setdefault('Cover_Image', '') # May not be necessary
+
+
+    #json_input.setdefault('Author_First_Name_2', '') # May not be necessary
+    #json_input.setdefault('Author_Last_Name_2', '') # May not be necessary
+
+
+    owned_value = json_input.setdefault('Owned', 'no')
+    favorite_value = json_input.setdefault('Favorite', 'no')
+    completed_value = json_input.setdefault('Completed', 'no')
+    currently_reading_value = json_input.setdefault('Currently_Reading', 'no')
+
+    # If the tags are present in JSON we need to convert from frontend syntax (on, off) to backend (yes, no)
+    tag_list = [('Owned', owned_value), ('Favorite', favorite_value), ('Completed', completed_value),
+                ('Currently_Reading', currently_reading_value)]
+    for key, tag in tag_list:
+        match tag:
+            case 'on':
+                json_input.update({key: 'yes'})
+            case _:
+                json_input.update({key: 'no'})
+
+    json_input.setdefault('Personal_Or_Academic', 'personal') # Will default to personal
+
+    json_input.setdefault('Genre_1', 'fiction') # Will default Genre 1 to fiction
+    json_input.setdefault('Genre_2', '')
+    json_input.setdefault('Genre_3', '')
+    json_input.setdefault('Genre_4', '')
+
+    return json_input
+
+
+def validate_refactor(json_input):
+    tag_values = ['yes', 'no']
+
+    try:
+        constructed_json = ''
+        for key, value in json_input.items():
+            match key, value:
+
+                # Check Book Information
+                case ('ISBN', value) if len(value) == 10 or len(value) == 13:
+                    # Makes sure isbn is a string
+                    json_input['ISBN'] = str(json_input['ISBN']).strip()
+                    constructed_json += 'ISBN'
+
+                case ('Title', value) if len(value) > 0:
+                    # Strip whitespace and make title capitalization
+                    json_input['Title'] = json_input['Title'].strip().title()
+                    constructed_json += 'Title'
+
+                case ('Chapters', value) if len(value) >= 0:
+                    json_input['Chapters'] = str(json_input['Chapters']).strip()
+                    constructed_json += 'Chapters'
+
+                case ('Chapters_Completed', value) if len(value) >= 0 and value <= json_input['Chapters']:
+                    json_input['Chapters_Completed'] = str(json_input['Chapters_Completed']).strip()
+                    constructed_json += 'Chapters_Completed'
+
+                case ('Summary', value) if len(value) >= 0:
+                    json_input['Summary'] = json_input['Summary'].strip()
+                    constructed_json += 'Summary'
+
+                case ('Cover_Image', value) if len(value) > 0:
+                    json_input['Cover_Image'] = json_input['Cover_Image'].strip()
+                    constructed_json += 'Cover_Image'
+
+
+                # Check Author Information
+                case ('Author_First_Name_1', value) if len(value) > 0:
+                    json_input['Author_First_Name_1'] = json_input['Author_First_Name_1'].strip()
+                    constructed_json += 'Author_First_Name_1'
+
+                case ('Author_Last_Name_1', value) if len(value) > 0:
+                    json_input['Author_Last_Name_1'] = json_input['Author_Last_Name_1'].strip()
+                    constructed_json += 'Author_Last_Name_1'
+
+                # If there is a second author, if no key:value pair exists book record will ignore the lack of value
+                # Will only validate if a second author is present
+                case ('Author_First_Name_2', value) if len(value) > 0:
+                    json_input['Author_First_Name_2'] = json_input['Author_First_Name_2'].strip()
+                    constructed_json += 'Author_First_Name_2'
+
+                case ('Author_Last_Name_2', value) if len(value) > 0:
+                    json_input['Author_Last_Name_2'] = json_input['Author_Last_Name_2'].strip()
+                    constructed_json += 'Author_Last_Name_2'
+
+
+                # Check Publisher Information
+                case ('Publish_Year', value) if len(value) > 0:
+                    json_input['Publish_Year'] = json_input['Publish_Year'].strip()
+                    constructed_json += 'Publish_Year'
+
+                case ('Publisher_Name', value) if len(value) > 0:
+                    json_input['Publisher_Name'] = json_input['Publisher_Name'].strip()
+                    constructed_json += 'Publisher_Name'
+
+                # Tag Information
+                case ('Owned', value) if value.lower() in tag_values:
+                    json_input['Owned'] = json_input['Owned'].lower()
+                    constructed_json += 'Owned'
+
+                case ('Favorite', value) if value.lower() in tag_values:
+                    json_input['Favorite'] = json_input['Favorite'].lower()
+                    constructed_json += 'Favorite'
+
+                case ('Completed', value) if value.lower() in tag_values:
+                    json_input['Completed'] = json_input['Completed'].lower()
+                    constructed_json += 'Completed'
+
+                case ('Currently_Reading', value) if value.lower() in tag_values:
+                    json_input['Currently_Reading'] = json_input['Currently_Reading'].lower()
+                    constructed_json += 'Currently_Reading'
+
+                case ('Personal_Or_Academic', value)  if value.lower() in ['personal', 'academic']:
+                    json_input['Personal_Or_Academic'] = json_input['Personal_Or_Academic'].lower()
+                    constructed_json += 'Personal_Or_Academic'
+
+
+                # Genre Information
+                case ('Genre_1', value) if value.lower() in ['fiction', 'nonfiction']:
+                    json_input['Genre_1'] = json_input['Genre_1'].lower()
+                    constructed_json += 'Genre_1'
+
+                case ('Genre_2', value) if len(value) > 0: # Temp until genre list is finished
+                    json_input['Genre_2'] = json_input['Genre_2'].lower()
+                    constructed_json += 'Genre_2'
+
+                case ('Genre_3', value) if len(value) > 0: # Temp until genre list is finished
+                    json_input['Genre_3'] = json_input['Genre_3'].lower()
+                    constructed_json += 'Genre_3'
+
+                case ('Genre_4', value) if len(value) > 0: # Temp until genre list is finished
+                    json_input['Genre_4'] = json_input['Genre_4'].lower()
+                    constructed_json += 'Genre_4'
+
+
+                case _:  'False'
+
+            constructed_json += '\n'
+
+        #return constructed_json
+        return json_input
+
+    except TypeError and ValueError:
+        return f'Error: Invalid Entry, could not parse. Try again.', 400
+
+
+
+
+#first = add_missing_keys(normal_data)
+#print(first)
+create_book(normal_data)
+result = read_book('0061091464')
 print(result)
-
-
-
+#print(result)
+#refactor_result = validate_refactor(first)
+#print(refactor_result)
 
 
 
