489ae7bd8a991448d7e7cac0ac37e6576201d228
Collin Geersen <cpgeersen@my.waketech.edu>
2026-03-23 11:06:51 -0400

    add: initial note commit

--- app/services/Book/Book.py
+++ app/services/Book/Book.py
@@ -3,6 +3,7 @@ from app.services.Book.BookRead import (read_full_book_record, get_all_book_isbn
                                         read_full_book_by_author)
 from app.services.Book.BookUpdate import update_summary, update_chapters, update_read_chapters, update_tags
 from app.services.Book.BookDelete import delete_book_record
+from app.services.Book.BookNotes import create_note, read_note, update_note, delete_note
 from app.services.Book.BookPredicate import is_isbn_in_book_table
 import json
 
@@ -34,7 +35,6 @@ def read_all_books_by_title(title):
 def read_all_books_by_author(author_last_name, author_first_name=None):
     return read_full_book_by_author(author_last_name, author_first_name)
 
-
 def update_book_summary(isbn, summary):
     return update_summary(isbn, summary)
 
@@ -47,11 +47,36 @@ def update_book_chapters_completed(isbn, chapters_completed):
 def update_book_tags(tag_id, owned, favorite, completed, currently_reading):
     return update_tags(tag_id, owned, favorite, completed, currently_reading)
 
-
-
-
 def delete_book(isbn):
     return delete_book_record(isbn)
+
+def create_book_note(json_input):
+    return create_note(json_input)
+
+def read_book_notes(json_input):
+    return read_note(json_input)
+
+def update_book_note(json_input):
+    return update_note(json_input)
+
+def delete_book_note(json_input):
+    return delete_note(json_input)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
