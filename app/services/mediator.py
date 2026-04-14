16e937c8970498f17377a9e0e53893902ffec5d3
Collin Geersen <cpgeersen@my.waketech.edu>
2026-04-14 01:49:51 -0400

    refactor: mediator formatting

--- app/services/mediator.py
+++ app/services/mediator.py
@@ -1,24 +1,23 @@
 from app.services.Mediator.mediator_create import mediator_create
-from app.services.Mediator.mediator_delete import mediator_delete
 from app.services.Mediator.mediator_read import mediator_read
 from app.services.Mediator.mediator_update import mediator_update
+from app.services.Mediator.mediator_delete import mediator_delete
 
-SUCCESS = 200
-FOUND = 302
-BAD_REQUEST = 400
-INTERNAL_SERVER_ERROR = 500
 
 # POST - Takes JSON as input
 def create(json_input, create_type):
-   return mediator_create(json_input, create_type)
+    # OpenLibrary Functions Called In Inner Function
+    return mediator_create(json_input, create_type)
 
 # GET - Takes JSON as input
 def read(json_input=None, read_type='book-all', filter_json=None):
+    # OpenLibrary Functions Called In Inner Function
     return mediator_read(json_input, read_type, filter_json)
 
 
 # PATCH - Takes JSON as input
 def update(json_input, update_type):
+    # OpenLibrary Functions Called In Inner Function
     return mediator_update(json_input, update_type)
 
 
