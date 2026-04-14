667d2d52d32beeb28d3156db2b9343af6cbe452a
Collin Geersen <cpgeersen@my.waketech.edu>
2026-04-14 03:17:37 -0400

    refactor: main imports

--- app/main.py
+++ app/main.py
@@ -1,19 +1,18 @@
-import os
-import json
-from pathlib import Path
-from flask import request, render_template, jsonify, redirect, url_for, send_from_directory
+from flask import render_template, send_from_directory
 
+# Route imports
 from app.routes.add_local_book_route import add_local_book_route
 from app.routes.individual_book_route import individual_book_route
 from app.routes.local_search_route import local_search_route
 from app.routes.openlibrary_search_route import openlibrary_search_route
 from app.services.OpenLibrary.openlibrary_search_cache import create_cache
-from app.services.mediator import create, read, update, delete
+
+# Test Route Import
 from app.routes.test import test_bp
-from app.services.genres import genres_for_table
+
+# Imports used for Mocking
 from app.services.mocking.create_example_records import create_sample_books
 from app.services.mocking.create_many_records import create_many_records
-
 
 
 # Status Codes
